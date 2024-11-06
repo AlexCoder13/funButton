fea5b022162da8fb0e3f10f9e6704352e390fdd7
Александр Семёнов <aleksandrsemenov@iMac-Aleksandr.local>
2024-11-06 23:03:56 +0300

    ver 2.0

--- macOStest/ContentView.swift
+++ macOStest/ContentView.swift
@@ -8,70 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var firstText = "Кто ты на самом деле?"
-    @State var toggle = false
+    @State var mainTitle = "КУДА ПОЙДЕМ ?"
+    @State var toggleIsOn1 = false
+    @State var toggleIsOn2 = false
+    @State var colorChoice1 = false
+    @State var colorChoice2 = false
     @State var button = true
-    @State var color = false
-    @State var colorSecond = false
     var body: some View {
-        someView()
-    }
-    
-    func result() {
-        if Bool.random() {
-            firstText = "SIGMA Чел"
-            toggle = false
-            button = false
-            color = true
-            colorSecond = false
-        } else {
-            firstText = "ГОМОГЕЕЕЕЕЕЕЕЕЕЕЙ"
-            toggle = true
-            button = false
-            color = false
-            colorSecond = true
+        ZStack {
+            backGround()
+            foreGround()
         }
     }
     
-    func someView() -> some View {
+    func result() {
+        button = false
+        if Bool.random() {
+            mainTitle = "ЗАКЛАДКУ НАЙДЕШЬ"
+            toggleIsOn1 = false
+            toggleIsOn2 = true
+            colorChoice1 = true
+            colorChoice2 = false
+        } else {
+            mainTitle = "В КАКАШКУ НАСТУПИШЬ"
+            toggleIsOn1 = true
+            toggleIsOn2 = false
+            colorChoice1 = false
+            colorChoice2 = true
+        }
+    }
+    
+    func backGround() -> some View {
         ZStack {
-            Color.pink
-            if color {
+            Color.gray
+            if colorChoice1 {
                 Color.black
             }
-            if colorSecond {
+            if colorChoice2 {
                 VStack(spacing: 0) {
-                    Color.red
-                    Color.orange
-                    Color.yellow
-                    Color.green
-                    Color.blue
-                    Color.purple
+                    Color.brown
                 }
             }
-            VStack(spacing: 5) {
-                if toggle {
-                    Text("А я всегда знал, что ты")
-                }
-                Text(firstText)
-                if button {
+        }
+    }
+    func foreGround() -> some View {
+        VStack(spacing: 5) {
+            if toggleIsOn1 {
+                Text("НАЛЕВО ПОЙДЕШЬ - ")
+            }
+            if toggleIsOn2 {
+                Text("НАПРАВО ПОЙДЕШЬ - ")
+            }
+            Text(mainTitle)
+            if button {
                 Button {
                     result()
                 }
-                label:  {
-                    Text("ПРОВЕРИТЬ")
-                }
+            label:  {
+                Text("ПРОВЕРИТЬ")
             }
             }
         }
         .frame(width: 200)
-        .padding()
-    }
+        .padding()    }
+    
 }
+
+
+
 
 #Preview {
     ContentView()
 }
+
+
+
 
 
 // VStack
@@ -144,7 +155,7 @@ struct ContentView: View {
 //        .frame(width: 200)
 //        .padding()
 //    }
-//    
+//
 //    func result() {
 //        if Bool.random() {
 //            firstText = "SIGMA Чел"
